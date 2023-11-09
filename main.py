d0663f4316a3d8815706b2a841136747608ab1b5
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-11-08 21:30:48 -0500

    Updated to handle error checks, next step: AWS

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import tweepy
 import requests
-import json
-from config import api_key, api_secret_key,bearer_token, access_token, access_token_secret, quote_key, api_url
+from config import api_key, api_secret_key, bearer_token, access_token, access_token_secret, quote_key, api_url
 
+client = tweepy.Client(bearer_token, consumer_key=api_key, consumer_secret=api_secret_key,
+                       access_token=access_token, access_token_secret=access_token_secret)
 
-client = tweepy.Client(bearer_token,api_key, api_secret_key, access_token, access_token_secret)
-auth = tweepy.OAuth1UserHandler(api_key, api_secret_key, access_token, access_token_secret)
-api = tweepy.API(auth)
+def get_quote():
+    response = requests.get(api_url, headers={'X-Api-Key': quote_key})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        if data:
+            item = data[0]
+            quote = item['quote']
+            author = item['author']
+            return quote, author
+    return None, None
 
+def attempt_to_tweet(max_attempts=5):
+    attempts = 0
+    while attempts < max_attempts:
+        quote, author = get_quote()
+        if quote is not None:
+            tweet_text = f'"{quote}" - {author}'
+            if len(tweet_text) <= 280:
+                try:
+                    client.create_tweet(text=tweet_text)
+                    print(f"Tweeted successfully: {tweet_text}")
+                    return
+                except tweepy.TweepyException as e:
+                    print(f"Failed to tweet: {e}")
+                    
+            else:
+                print(f"Quote is too long ({len(tweet_text)} characters), getting a new one...")
+        else:
+            print("Failed to get a new quote.")
+        attempts += 1
+    print("Max tweet attempts reached.")
 
-response = requests.get(api_url, headers={'X-Api-Key': quote_key})
+def main():
+    attempt_to_tweet()
 
-if response.status_code == requests.codes.ok:
-    print(response.text)
-    data = response.json()
-    item = data[0]
-    quote = item['quote']
-    author = item['author']
-    print(quote)
-    print(author)
-    tweet_text = f'"{quote}" - {author}'
-    client.create_tweet(text = tweet_text)
-else:
-    print("Error:", response.status_code, response.text)
-
-
+if __name__ == "__main__":
+    main()
